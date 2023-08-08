a50e39bab6f659f4c88866ed85d641e0d1089ae0
julieprunaret <j.prunaret@gmail.com>
2023-08-08 10:03:46 +0200

    fix css and js elements

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-
-
 const input = document.querySelector('input[name="email"]');
 const errorMessage = document.querySelector('.error-message');
 
-input.addEventListener('invalid', function (event) {
-  if (event.target.validity.valueMissing) {
-    
+input.addEventListener('invalid', (event) => {
+  if (event.target.validity) {  
     event.preventDefault();
-    errorMessage.textContent = 'The field cannot be empty';
-  } else {
-    
-    event.preventDefault();
-    errorMessage.textContent = 'The email address is not formatted correctly';
-  }
+    input.classList.add("errorInput");
+    if (event.target.validity.valueMissing) {
+      errorMessage.textContent = 'Email required.';
+    } else {
+      errorMessage.textContent = 'Valid email required.';
+    }
+  } 
 })
